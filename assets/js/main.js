fcce2847f7df59836d7898259b8764a5181ff18d
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-06-13 16:07:31 +0100

    Add a temporary function to hide game card

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// Temporary function to hide .game-card-info while building the game
+$(".game-card-show").click(() => {
+  $(".game-card-show").addClass("game-card-hide");
+  console.log("It disappeared");
+});
