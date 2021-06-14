558bb785162ebc15a0ae35e07bb41e3ab3180a49
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-06-14 13:49:48 +0100

    Add 4 variables assigned to elements in index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,8 @@ $(".game-card-show").click(() => {
   $(".game-card-show").addClass("game-card-hide");
   console.log("It disappeared");
 });
+
+const holes = $(".hole");
+const scoreBoard = $(".current-score");
+const moles = $(".mole");
+const timer = $(".timer")
